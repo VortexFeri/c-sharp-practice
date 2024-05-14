5de48162c6daa0f7d7897b773892fb950059a56e
VortexFeri <fericarnu@gmail.com>
2024-05-15 01:52:13 +0300

    rewritten Account class with serialization and new error types

--- ticket_purchaser/Account.cs
+++ ticket_purchaser/Account.cs
@@ -1,36 +1,145 @@
-﻿using res;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using res;
 
-namespace user
+namespace user_namespace
 {
-    public class Account(string name, string password, Account.Roles role = Account.Roles.User)
+    [method: JsonConstructor]
+    public class Account(string name, string password, Role role = Role.User)
     {
+        [JsonInclude]
+        [JsonPropertyName("username")]
         public string Name { get; } = name;
-        private int Balance { get; } = 0;
+
+        [JsonInclude]
+        [JsonPropertyName("credits")]
+        private int Balance { get; }
+
+        [JsonInclude]
+        [JsonPropertyName("password")]
         private readonly string Password = password;
-        private readonly Roles Role = role;
 
-        public bool CheckPassword(string password) => Password != password;
+        [JsonInclude]
+        [JsonPropertyName("role")]
+        public readonly Role Role = role;
 
-        public enum Roles
+        public bool CheckPassword(string password) => Password == password;
+    }
+
+    public class LoginManager
+    {
+        private List<Account> accounts = [];
+        private readonly string filePath;
+
+        public LoginManager(string filePath)
         {
-            Admin,
-            User,
-            Superuser
+            this.filePath = filePath;
+            LoadAccounts();
         }
 
-        readonly struct Error
+        private bool AccountExists(string username)
         {
-            internal enum UserError
+            return accounts.Exists(acc => acc.Name == username);
+        }
+
+        public Result<Account, ResultError<LoginError>> Match(string username, string password)
+        {
+            if (!AccountExists(username)) return new(Error.UserNotFound);
+            var account = accounts.Find(acc => acc.Name == username);
+            if (account!.CheckPassword(password))
             {
-                UserNotFound,
-                UserAlreadyExists,
-                UserNotAuthorized,
-                InvalidPassword
+                return new(account);
             }
-            public static readonly Error<UserError> UserNotFound = new(UserError.UserNotFound, "The user does not exist.");
-            public static readonly Error<UserError> UserAlreadyExists = new(UserError.UserAlreadyExists, "This user already exists");
-            public static readonly Error<UserError> UserNotAuthorised = new(UserError.UserNotAuthorized, "You are not allowed to do this operation.");
-            public static readonly Error<UserError> InvalidPassword = new(UserError.InvalidPassword, "The entered password is invalid");
+            else
+            {
+                return new(Error.InvalidPassword);
+            }
         }
+
+
+        private void LoadAccounts()
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                accounts = JsonSerializer.Deserialize<List<Account>>(json) ?? [];
+            }
+            catch (FileNotFoundException)
+            {
+                // If the file does not exist, initialize accounts list
+                accounts = [];
+            }
+            catch (JsonException e)
+            {
+                // If there's an error parsing JSON, initialize accounts list
+                Console.WriteLine("Error loading accounts from file. Initializing empty accounts list.");
+                Console.WriteLine(e.Message);
+                accounts = [];
+            }
+        }
+
+        private void SaveAccounts()
+        {
+            string json = JsonSerializer.Serialize(accounts);
+            File.WriteAllText(filePath, json);
+        }
+
+        public Result<Account, ResultError<SignUpError>> Register(string username, string password)
+        {
+            if (AccountExists(username))
+            {
+                Console.WriteLine("Username already exists. Please choose a different username.");
+                return new(Error.UserAlreadyExists);
+            }
+
+            var newAccount = new Account(username, password);
+            accounts.Add(newAccount);
+            SaveAccounts();
+            Console.WriteLine("Registration successful.");
+            return new(newAccount);
+        }
+
+    }
+
+    public enum Role
+    {
+        User, 
+        Admin, //TODO: Admin role
+        Superuser //TODO: Superuser role
+    }
+
+    public enum SignUpError
+    {
+        UserAlreadyExists,
+        InvalidCharacter, //TODO: Check for invalid characters in username
+        WeakPassword //TODO: check password strength
+    }
+
+    public enum LoginError
+    {
+        InvalidPassword,
+        UserNotFound,
+    }
+
+    public enum UserOperationError
+    {
+        NotEnoughCredit,
+        UserNotAuthorized,
+        AdminNotAuthorized
+    }
+
+    public readonly struct Error
+    {
+        public static readonly ResultError<SignUpError> UserAlreadyExists = new(SignUpError.UserAlreadyExists, "This user already exists.");
+        public static readonly ResultError<SignUpError> InvalidCharacter = new(SignUpError.InvalidCharacter, "The username contains invalid characters.");
+        public static readonly ResultError<SignUpError> WeakPassword = new(SignUpError.WeakPassword, "The password is too weak.");
+
+
+        public static readonly ResultError<LoginError> InvalidPassword = new(LoginError.InvalidPassword, "The entered password is invalid.");
+        public static readonly ResultError<LoginError> UserNotFound = new(LoginError.UserNotFound, "The user does not exist.");
+
+        public static readonly ResultError<UserOperationError> NotEnoughCredit = new(UserOperationError.NotEnoughCredit, "Insufficient funds.");
+        public static readonly ResultError<UserOperationError> UserNotAuthorised = new(UserOperationError.UserNotAuthorized, "You are not allowed to do this operation.");
+        public static readonly ResultError<UserOperationError> AdminNotAuthorised = new(UserOperationError.AdminNotAuthorized, "Only The SuperUser can do that.");
     }
 }
